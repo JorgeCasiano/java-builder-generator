3ea19142ff5bd150a494887dbd03762f9e206ee2
kas7ux <kas7ux@gmail.com>
2022-08-20 08:38:38 +0200

    feat: create builder processor

---
+++ java-builder/src/main/java/com/casiano/builder/BuilderProcessor.java
@@ -0,0 +1,160 @@
+package com.casiano.builder;
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+@SupportedAnnotationTypes("com.casiano.builder.Builder")
+@AutoService(Processor.class)
+public class BuilderProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Builder.class);
+
+        elements.forEach(this::generateClass);
+
+        return false;
+    }
+
+    private void generateClass(Element element) {
+        try {
+            String pkg = element.getEnclosingElement().toString();
+
+            String builderName = element.getSimpleName() + "Builder";
+
+            JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(pkg + "." + builderName);
+            PrintWriter out = new PrintWriter(builderFile.openWriter());
+            try (out) {
+                declareClass(out, pkg, builderName);
+                List<FieldInfo> fields = extractFields(element);
+                addFields(fields, out);
+
+                addBuildMethod(out, pkg, element.getSimpleName().toString(), fields);
+                addMethods(fields, builderName, out);
+                addHelperSetField(out, pkg, element.getSimpleName().toString());
+
+                out.println("}");
+            }
+        } catch (Exception e) {
+            error("Error: " + e, element);
+        }
+    }
+
+    private List<FieldInfo> extractFields(Element element) {
+        return element.getEnclosedElements().stream()
+                .filter(e -> e.getKind() == ElementKind.FIELD)
+                .map(e -> new FieldInfo(e.getSimpleName().toString(), e.asType().toString()))
+                .toList();
+    }
+
+    private void declareClass(PrintWriter out, String pkg, String builderName) {
+        out.println("package " + pkg + ";");
+        lineBreak(out);
+        out.println("public class " + builderName + "{");
+        lineBreak(out);
+    }
+
+    private void addBuildMethod(PrintWriter out, String pkg, String clsName, List<FieldInfo> fields) {
+        String fullName = pkg + "." + clsName;
+        String variable = clsName.toLowerCase();
+        addIndentation(out, 1);
+        out.println("public " + fullName + " build() {");
+        addIndentation(out, 2);
+        out.println("try {");
+        addIndentation(out, 3);
+        out.println("java.lang.reflect.Constructor<" + fullName + "> constructor =");
+        addIndentation(out, 4);
+        out.println("java.util.Optional.ofNullable(" + fullName + ".class.getConstructor())");
+        addIndentation(out, 5);
+        out.println(".orElseThrow(() -> new RuntimeException(\"default constructor is required in class " + fullName + "\"));");
+        addIndentation(out, 3);
+        out.println(fullName + " " + variable + " = constructor.newInstance();");
+        addIndentation(out, 3);
+        out.println("java.lang.reflect.Field[] fields = Book.class.getDeclaredFields();");
+        for (int i = 0; i < fields.size(); i++) {
+            addIndentation(out, 3);
+            out.println("setField(" + variable + ", fields[" + i + "], this." + fields.get(i).name + ");");
+        }
+        addIndentation(out, 3);
+        out.println("return " + variable + ";");
+        addIndentation(out, 2);
+        out.println("} catch (NoSuchMethodException | InstantiationException | IllegalAccessException | java.lang.reflect.InvocationTargetException e) {");
+        addIndentation(out, 3);
+        out.println("throw new RuntimeException(e);");
+        addIndentation(out, 2);
+        out.println("}");
+        addIndentation(out, 1);
+        out.println("}");
+
+        lineBreak(out);
+    }
+
+    private void addFields(List<FieldInfo> fields, PrintWriter out) {
+        fields.forEach(field -> {
+            addIndentation(out, 1);
+            out.println("private " + field.type + " " + field.name + ";");
+            lineBreak(out);
+        });
+        lineBreak(out);
+    }
+
+    private void addMethods(List<FieldInfo> fields, String builderName, PrintWriter out) {
+        fields.forEach(field -> this.addMethod(field, builderName, out));
+        lineBreak(out);
+    }
+
+    private void addMethod(FieldInfo field, String builderName, PrintWriter out) {
+        addIndentation(out, 1);
+        out.println("public " + builderName + " " + field.name + "(" + field.type + " " + field.name + ") {");
+
+        addIndentation(out, 2);
+        out.println("this." + field.name + " = " + field.name + ";");
+        addIndentation(out, 2);
+        out.println("return this;");
+
+        addIndentation(out, 1);
+        out.println("}");
+    }
+
+    private void lineBreak(PrintWriter out) {
+        out.println();
+    }
+
+    private void addIndentation(PrintWriter out, int level) {
+        IntStream.range(0, level).forEach(l -> out.print("  "));
+    }
+
+    private void addHelperSetField(PrintWriter out, String pkg, String clsName) {
+        String fullName = pkg + "." + clsName;
+        String parameter = clsName.toLowerCase();
+        addIndentation(out, 1);
+        out.println("private void setField(" + fullName + " " + parameter + " , java.lang.reflect.Field field, Object value) throws IllegalAccessException {");
+        addIndentation(out, 2);
+        out.println("field.setAccessible(true);");
+        addIndentation(out, 2);
+        out.println("field.set(" + parameter + ", value);");
+        addIndentation(out, 1);
+        out.println("}");
+    }
+
+    private void error(String msg, Element e) {
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, msg, e);
+    }
+
+    record FieldInfo(String name, String type) {
+
+    }
+}
